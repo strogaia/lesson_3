39460dbd75bae90b5013d538f9786aa49d3c2e42
Strogaia Nadejda <52749046+strogaia@users.noreply.github.com>
2019-07-17 13:56:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 n = 1
 // pattern
-
+// так  
+// n = 1
+// while( n <= 10 ){ 
+//   if( ( n % 2 ) == 0){ document.write("#")
+//   } else{
+//     document.write("*")
+//   }
+//   n++
+// }
+// или так
 // 1.*#*#*#*#*#
 // while( n <= 10 ){
 //   if( n == 2 ){
@@ -20,6 +29,16 @@ n = 1
 // }
 
 // 2.**#**#**#*
+// так  
+// n = 1
+// while( n <= 10 ){ 
+//   if( ( n % 3 ) == 0){ document.write("#")
+//   } else{
+//     document.write("*")
+//   }
+//   n++
+// }
+// или так
 // while( n <= 10 ){
 //   if( n == 3 ){
 //     document.write("#")
@@ -36,41 +55,55 @@ n = 1
 // 3.*#*#*#*#*#
 //   #*#*#*#*#*
 //   *#*#*#*#*#
-while( n <= 31 ){
-  if( n == 2 ){
-    document.write("#")
-  } else if ( n == 4) {
-    document.write("#")
-  } else if ( n == 6) {
-    document.write("#")
-  } else if ( n == 8) {
-    document.write("#")
-  } else if ( n == 10) {
-    document.write("#")
-  } else if ( n == 11){
-    document.write('<br>' + "#")
-  } else if ( n == 13){
-    document.write("#")
-  } else if ( n == 15){
-    document.write("#")
-  } else if ( n == 17){
-    document.write("#")
-  } else if ( n == 19){
-    document.write("#")
-  } else if ( n == 21){
+// так 
+while( n <= 30 ){ 
+  if (( n % 20) == 0){ 
     document.write('<br>')
-  } else if ( n == 23) {
+  } else if(( n % 10 ) == 0){ 
+    document.write("# " + '<br>' +"#")
+  } else if(( n % 2) == 0){ 
     document.write("#")
-  } else if ( n == 25) {
-    document.write("#")
-  } else if ( n == 27) {
-    document.write("#")
-  } else if ( n == 29) {
-    document.write("#")
-  } else if ( n == 31) {
-    document.write("#")
-  } else {
+  } else{
     document.write("*")
-  }
-  n++
+    }
+  
+n++  
 }
+// while( n <= 31 ){
+//   if( n == 2 ){
+//     document.write("#")
+//   } else if ( n == 4) {
+//     document.write("#")
+//   } else if ( n == 6) {
+//     document.write("#")
+//   } else if ( n == 8) {
+//     document.write("#")
+//   } else if ( n == 10) {
+//     document.write("#")
+//   } else if ( n == 11){
+//     document.write('<br>' + "#")
+//   } else if ( n == 13){
+//     document.write("#")
+//   } else if ( n == 15){
+//     document.write("#")
+//   } else if ( n == 17){
+//     document.write("#")
+//   } else if ( n == 19){
+//     document.write("#")
+//   } else if ( n == 21){
+//     document.write('<br>')
+//   } else if ( n == 23) {
+//     document.write("#")
+//   } else if ( n == 25) {
+//     document.write("#")
+//   } else if ( n == 27) {
+//     document.write("#")
+//   } else if ( n == 29) {
+//     document.write("#")
+//   } else if ( n == 31) {
+//     document.write("#")
+//   } else {
+//     document.write("*")
+//   }
+//   n++
+// }
