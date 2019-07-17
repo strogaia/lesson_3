86eab64ca435d6af2572c6aa3f378c6d7ecd4248
strogaia <nadejda.screbliucova@gmail.com>
2019-07-17 12:20:06 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+n = 1
+// pattern
+
+// 1.*#*#*#*#*#
+// while( n <= 10 ){
+//   if( n == 2 ){
+//     document.write("#")
+//   } else if ( n == 4) {
+//     document.write("#")
+//   } else if ( n == 6) {
+//     document.write("#")
+//   } else if ( n == 8) {
+//     document.write("#")
+//   } else if ( n == 10) {
+//     document.write("#")
+//   } else {
+//   document.write("*")
+//   }
+//   n++
+// }
+
+// 2.**#**#**#*
+// while( n <= 10 ){
+//   if( n == 3 ){
+//     document.write("#")
+//   } else if ( n == 6) {
+//     document.write("#")
+//   } else if ( n == 9) {
+//     document.write("#")
+//   } else {
+//   document.write("*")
+//   }
+//   n++
+// }
+
+// 3.*#*#*#*#*#
+//   #*#*#*#*#*
+//   *#*#*#*#*#
+while( n <= 31 ){
+  if( n == 2 ){
+    document.write("#")
+  } else if ( n == 4) {
+    document.write("#")
+  } else if ( n == 6) {
+    document.write("#")
+  } else if ( n == 8) {
+    document.write("#")
+  } else if ( n == 10) {
+    document.write("#")
+  } else if ( n == 11){
+    document.write('<br>' + "#")
+  } else if ( n == 13){
+    document.write("#")
+  } else if ( n == 15){
+    document.write("#")
+  } else if ( n == 17){
+    document.write("#")
+  } else if ( n == 19){
+    document.write("#")
+  } else if ( n == 21){
+    document.write('<br>')
+  } else if ( n == 23) {
+    document.write("#")
+  } else if ( n == 25) {
+    document.write("#")
+  } else if ( n == 27) {
+    document.write("#")
+  } else if ( n == 29) {
+    document.write("#")
+  } else if ( n == 31) {
+    document.write("#")
+  } else {
+    document.write("*")
+  }
+  n++
+}
